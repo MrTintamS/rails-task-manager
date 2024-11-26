3c38141966ecf3060b8b354da73bd1f5f4f4ef8f
(martin selle) (atelier.martin.selle@gmail.com) <atelier.martin.selle@gmail.com>
2024-11-26 14:46:29 +0100

    EXO02-RAILSTASK-MANAGER

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,14 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  root "tasks#index"
+
   # root "posts#index"
+  get 'tasks' => 'tasks#index' , as: :tasks
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id' , to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
